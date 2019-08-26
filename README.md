4d879f1a64dede05179647061858ecef3871b245
RafaelMedeiros <rafaelmedeiros@maisentregas.com>
2019-08-26 08:57:15 -0300

    Finalização do projeto

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # CVNode
+
+Projeto de criação de um CV online usando nodeJs.
+
+Link do tutorial: https://woliveiras.com.br/posts/criando-um-curr%C3%ADculo-com-node-js-express-e-templates-ejs/?fbclid=IwAR3vFcRlA8QSaqp_z4ODcEQTvrkFwCFu6LImoNwNT76NwStSu-4HpGaoKGw#OqueoExpress
+
+Pacotes a seram instalados
+
+Framework para nodeJs
+npm install --save express
+
+Framework para renderização de páginas
+npm install --save ejs
+
+Componente de Hot-reload
+npm install --save-dev nodemon
+
+Tratamento de erros
+npm install --save http-errors
---
+++ models/curriculo-model.js
@@ -0,0 +1,22 @@
+const data = {
+    title: 'Meu currículo',
+    name: 'Rafael Tadeu',
+    profession: 'Programador',
+    description: 'Experiência em desenvolvimento full steak mobile com frameworks JavaScript (Vue), Grails(Java), MySql, Git.',
+    experience: [{
+        company: 'MaisEntregas.com',
+        office: 'Programador',
+        description: 'Experiência em desenvolvimento full steak mobile com frameworks JavaScript (Vue), Grails(Java), MySql, Git..'
+    },
+    {
+        company: 'HouseCom',
+        office: 'Professor',
+        description: 'Ministrar aulas de francês'
+    }],
+    education: [{
+        institution: 'PUC MINAS',
+        description: 'Bacharel em Ciência da Computação'
+    }],
+    skills: ['backend', 'frontend', 'infra', 'mobile']
+}
+module.exports = data;
